f617f3002aaa6e1d8541779ee12b9f14f512fcca
Diegoolei <diegooleiarz@hotmail.com>
2023-09-18 18:30:03 -0300

    Binary image generator, only float32 dimentions

--- cloud_read.py
+++ cloud_read.py
@@ -2,16 +2,59 @@ from scipy.io import FortranFile
 import matplotlib.pyplot as plt
 from constants import *
 import numpy as np
+import os
+import re  # Add the re import declaration to use regex
 
-f = FortranFile("outputdata1/nube3101.sal", "r")
-data = f.read_reals(np.float32)
-U1 = data[: biased_nx1 * biased_nx1 * biased_nz1].reshape(
-    (biased_nx1, biased_nx1, biased_nz1), order="F"
-)
-U2 = data[
-    biased_nx1 * biased_nx1 * biased_nz1: 2 * biased_nx1 * biased_nx1 *
-    biased_nz1].reshape((biased_nx1, biased_nx1, biased_nz1), order="C")
-plt.imshow(U1[:, :, 10])
-plt.savefig("img.png")
-print(len(data) / (biased_nx1 * biased_nx1 * biased_nz1))
-# .reshape((57, 57, 49), order="F")
+output_data_path = "outputdata/"
+var_amount = 12
+binary_regex = r"^nube31"
+plot_center = 10
+var_datatype = np.float32
+var_structure_size = biased_nx1 * biased_nx1 * biased_nz1
+var_structure = (biased_nx1, biased_nx1, biased_nz1)
+
+if os.path.exists(output_data_path):
+    if not os.path.exists("img/"):
+        os.makedirs("img/")
+    else:
+        print("The img folder already exists, files will be overwritten")
+        user_input = input("Press Y to continue or any other key to exit: ").upper()
+        if user_input != "Y":
+            exit()
+        else:
+            os.system("rm -rf img/*")
+
+    files = os.listdir(output_data_path)  # List all files in the outputdata folder
+    reg = re.compile(binary_regex)  # Compile the regex to match binary files
+    binary_files = list(
+        filter(reg.search, files)
+    )  # Create iterator using filter, cast to list
+    file_counter = 0
+    for file in binary_files:
+        f = FortranFile("outputdata/" + file, "r")
+        data = f.read_reals(var_datatype)
+        directory = "img/" + str(file_counter) + file.split(".")[0] + "/"
+        file_counter += 1
+        if not os.path.exists("img/" + str(file_counter) + "/"):
+            # TODO - Fix F77 filename strings, then replace file_counter with directory
+            os.makedirs("img/" + str(file_counter) + "/")
+        var_iterator = 0
+        for i in range(0, len(data), var_structure_size):
+            variable = data[i: i + var_structure_size].reshape(
+                (var_structure[0], var_structure[1], var_structure[2]), order="F"
+            )
+            plt.imshow(variable[:, :, plot_center])
+            plt.savefig(
+                "img/"
+                + str(file_counter)
+                + "/"
+                + nube31_var_list[var_iterator]
+                + ".png"
+            )
+            plt.close()
+            if var_iterator < var_amount:
+                var_iterator += 1
+            else:
+                var_iterator = 0
+else:
+    print("The outputdata folder does not exist")
--- constants.py
+++ constants.py
@@ -4,3 +4,18 @@ nz1 = 45
 biased_nx1 = nx1 + 7
 biased_nz1 = nz1 + 5
 nz1 = 45
+
+nube31_var_list = [
+    "U1",
+    "V1",
+    "W1",
+    "Titaa1",
+    "Pres1",
+    "Qvap1",
+    "Qgot1",
+    "Qllu1",
+    "Qcri1",
+    "Qnie1",
+    "Qgra1",
+    "aer1",
+]
