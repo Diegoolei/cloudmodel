9c5481666253b3a01eac940e6f215cd0ba5c7c14
Diegoolei <diegooleiarz@hotmail.com>
2024-07-17 20:30:52 -0300

    CI

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 """Setup file for the python package."""
 from setuptools import setup, find_packages
 
-setup(packages = find_packages("python"),)
+setup(packages = find_packages("python/*"),
+      include_package_data=True,
+      package_dir={"": "python"},)
