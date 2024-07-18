841e030c5ba331dea2269f0b566e5d09af7e8db6
Diegoolei <diegooleiarz@hotmail.com>
2024-07-18 02:08:12 -0300

    manifest fix

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,12 @@
 """Setup file for the python package."""
 from setuptools import setup, find_packages
 
+packages = find_packages()
+print(packages)
 setup(name = "cloudmodel", 
       version="0.2.15",
-      packages = find_packages(),
+      packages = packages,
       include_package_data=True,
-      package_dir={"cloudmodel": ""},
       package_data={
         '': ['*'],
     })
