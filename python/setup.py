742298cf8b2db6f11c96a96b9e49cc1bcdf9591b
Diegoolei <diegooleiarz@hotmail.com>
2024-07-18 02:22:20 -0300

    version

--- python/setup.py
+++ python/setup.py
@@ -5,6 +5,8 @@ from setuptools import setup, find_packages
 packages = find_packages()
 print(packages)
 setup(
+    name="cloudmodel",
+    version="0.2.16",
     packages=packages,
     include_package_data=True,
     package_data={
