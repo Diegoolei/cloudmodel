69500367369bdfb2a134c8d2f666a1727c46fba6
Diegoolei <diegooleiarz@hotmail.com>
2024-07-18 02:38:58 -0300

    merge

--- python/cloudmodel/__init__.py
+++ python/cloudmodel/__init__.py
@@ -1,1 +1,5 @@
 """init file for the cloudmodel module."""
+
+from .interface import interface
+
+__all__ = ["interface"]
--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,15 @@
 """Setup file for the python package."""
+
 from setuptools import setup, find_packages
 
-setup(packages = find_packages(),)
+packages = find_packages()
+print(packages)
+setup(
+    name="cloudmodel",
+    version="0.2.16",
+    packages=packages,
+    include_package_data=True,
+    package_data={
+        "": ["*"],
+    },
+)
