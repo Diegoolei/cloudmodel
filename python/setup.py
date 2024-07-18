04066fb66ec1d7333794908c9c2f4e8d7ae3ad45
Diegoolei <diegooleiarz@hotmail.com>
2024-07-18 02:17:47 -0300

    version

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,13 @@
 """Setup file for the python package."""
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
 print(packages)
-setup(name = "cloudmodel", 
-      version="0.2.15",
-      packages = packages,
-      include_package_data=True,
-      package_data={
-        '': ['*'],
-    })
+setup(
+    packages=packages,
+    include_package_data=True,
+    package_data={
+        "": ["*"],
+    },
+)
