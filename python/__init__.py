3b8f817d21f6d453b8b458048da4d9028f5ef617
Diegoolei <diegooleiarz@hotmail.com>
2024-07-18 00:27:36 -0300

    Prueba asiatico

---
+++ python/__init__.py
@@ -0,0 +1,3 @@
+from .cloudmodel import interface, cloudmodel
+
+__all__ = ["cloudmodel", "interface"]
--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,9 @@
 """Setup file for the python package."""
 from setuptools import setup, find_packages
 
-setup(packages = find_packages("python/*"),
+setup(packages = find_packages(),
       include_package_data=True,
-      package_dir={"": "python"},)
+      package_dir={"cloudmodel": ""},
+      package_data={
+        '': ['*'],
+    })
